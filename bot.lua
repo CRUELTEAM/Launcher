79a06c29d42b8682a36a6b06691edcb3d0fc7804
CRUEL TEAM <cruel@tarfandbazar.ir>
2016-08-22 20:37:12 +0430

    Update and rename launcher.lua to bot.lua

--- bot.lua
+++ bot.lua
@@ -7,7 +7,6 @@ HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
 local bot_api_key = "" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 188548712  --ID ADMIN ایدی خود را اینجا قرار دهید
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 function sendRequest(url)
   local dat, res = HTTPS.request(url)
